753966c3ef26072fc995aa0212e1201a308aa635
dscarrig <dcarrig1@gmail.com>
2021-03-27 19:07:04 -0500

    created

---
+++ js/nav.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+
+$body.on("click", "#nav-submit", navSubmitClick)
+
+function navSubmitClick(evt) {
+  console.log("SUBMIT");
+  hidePageComponents();
+  $submitStoryForm.show();
+}
+
+
+$body.on("click", "#nav-favorites", navFavoritesClick)
+
+function navFavoritesClick(evt) {
+  console.log("FAVORITES");
+}
+
+
+$body.on("click", "#nav-my-stories", navMyStoriesClick)
+
+function navMyStoriesClick(evt) {
+  console.log("MY STORIES");
+}
